65293159d84ba6d2c8f7391e289ceb2cf7954c81
immortalprogrammist <immortalvelial@icloud.com>
2023-03-20 14:42:46 +0100

    prepared project

---
+++ src/components/User.js
@@ -0,0 +1,5 @@
+function User(props) {
+  return <div></div>;
+}
+
+export default User;
---
+++ src/components/UserList.js
@@ -0,0 +1,11 @@
+import User from "./User";
+
+function UserList(props) {
+  return (
+    <div>
+      <User />
+    </div>
+  );
+}
+
+export default UserList;
